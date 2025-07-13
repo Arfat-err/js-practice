405f17e37572289693e57a8da6c2094f2c409c76
Arfat-err <arfatansari15398@gmail.com>
2025-07-13 20:58:56 +0530

    dom manipulation

---
+++ 8-dom/script.js
@@ -0,0 +1,15 @@
+// With the HTML DOM, JavaScript can access and change all the elements of an HTML document.
+// getElementById is a method, while innerHTML is a property.
+
+// The HTML DOM (Document Object Model)
+// When a web page is loaded, the browser creates a Document Object Model of the page.
+
+// The HTML DOM model is constructed as a tree of Objects:
+
+document.getElementById("button").onclick(){
+    document.body.style.background = "red";
+}
+
+document.getElementById("fheading").innerHTML = "Hello guy'z";
+// heading
+document.getElementById("fpara").innerHTML = "para is now change!"
